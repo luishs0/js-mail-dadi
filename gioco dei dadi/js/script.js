10e8edbf3dc5606a2465c4def8139069931804ee
luishs0 <luishs.doc@gmail.com>
2022-10-11 17:35:48 +0200

    esercizio finito

---
+++ gioco dei dadi/js/script.js
@@ -0,0 +1,25 @@
+let numberUser = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+let numberComputer = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+
+console.log(numberUser, numberComputer);
+
+let userMsg = document.querySelector(".numberUser");
+let computerMsg = document.querySelector(".numberComputer");
+
+userMsg.innerHTML = `Your number is ${numberUser}`;
+computerMsg.innerHTML = `The computer's number is ${numberComputer}`;
+
+let finalMsg = document.querySelector(".msg");
+
+let victory = false;
+
+
+if (numberUser > numberComputer) {
+    victory = true
+}
+
+if (victory) {
+    finalMsg.innerHTML = `<strong class="${victory}">YOU ARE THE WINNER</strong>`
+} else {
+    finalMsg.innerHTML = `<strong class="${victory}">YOU LOOSE</strong>`
+}

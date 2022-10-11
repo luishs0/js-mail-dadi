5da1df676877c42a0a291c9dd7d5160e6a59eb01
luishs0 <luishs.doc@gmail.com>
2022-10-11 16:49:41 +0200

    push

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,38 @@
-alert("ciao")
+const list = ["luis@boolean.com", "igor@boolean.com", "leo@boolean.com", "marco@boolean.com"]
+
+
+let mailUser = document.getElementById("mail");
+let submit = document.getElementById("submit");
+let answer = document.getElementById("answer")
+let isFound = false;
+
+
+
+submit.addEventListener("click", function() {
+    console.log(mailUser.value);
+    console.log(list.length);
+
+
+    for (let i = 0; i < list.length; i++) {
+        const thisElement = list[i];
+
+        
+       
+        if (thisElement === mailUser.value) {
+          isFound = true;
+          
+        } else {
+            isFound = false;
+        }
+    }
+    
+    if (isFound) {
+        answer.innerHTML = "mail corretto";
+    } else {
+        answer.innerHTML = "mail non corretto";
+    }
+})
+
+
+
+
